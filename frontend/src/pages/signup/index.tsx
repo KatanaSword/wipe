f638979f70ca49c1143da25d6e86b45f7ae85d92
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-09 19:35:39 +0530

    init commit

---
+++ frontend/src/pages/signup/index.tsx
@@ -0,0 +1,12 @@
+import { Form } from "react-hook-form";
+import { z } from "zod";
+
+const Signup = () => {
+  return (
+    <div className="">
+      <Form></Form>
+    </div>
+  );
+};
+
+export default Signup;
