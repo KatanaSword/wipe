a4859fb2b7a14ad4fd0996506dc57bd4092147ef
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-17 12:33:55 +0530

    user model added

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,132 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../type";
+import {
+  availableUserRole,
+  USER_TEMPORARY_TOKEN_EXPIRY,
+  userRoleEnum,
+} from "../constants";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      index: true,
+      trim: true,
+      minlength: 4,
+      maxlength: 20,
+      unique: true,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      index: true,
+      unique: true,
+      required: [true, "Email is required"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    avatar: {
+      type: {
+        url: String,
+        publicId: String,
+      },
+      default: {
+        url: "",
+        publicId: "",
+      },
+    },
+    role: {
+      type: String,
+      enum: availableUserRole,
+      default: userRoleEnum.USER,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      minlength: 12,
+      maxlength: 14,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+// To hash a password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next;
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
+});
+
+// To check a password
+userSchema.methods.isPasswordCorrect = async function (
+  password: string
+): Promise<boolean> {
+  const user = this as IUser;
+  return await bcrypt.compare(password, user.password);
+};
+
+// To generate a token
+userSchema.methods.generateAccessToken = function () {
+  const user = this as IUser;
+  return jwt.sign(
+    {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+    process.env.ACCESS_TOKEN_SECURE!,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRE! }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  const user = this as IUser;
+  return jwt.sign(
+    {
+      _id: user._id,
+    },
+    process.env.REFRESH_TOKEN_SECURE!,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRE! }
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken: string = crypto.randomBytes(20).toString("hex");
+  const hashedToken: string = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  const tokenExpiry: number = Date.now() + USER_TEMPORARY_TOKEN_EXPIRY;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
+
+export const User = model<IUser>("User", userSchema);
