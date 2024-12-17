e17770169f39314b32ead5ac2a34e560dc77c509
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-17 12:39:48 +0530

    more constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,15 @@
 export const DB_NAME: string = "wipe";
+
+export const userRoleEnum: { USER: string; ADMIN: string } = {
+  USER: "USER",
+  ADMIN: "ADMIN",
+};
+
+export const availableUserRole: string[] = Object.values(userRoleEnum);
+
+export const options: { httpOnly: boolean; secret: boolean } = {
+  httpOnly: true,
+  secret: true,
+};
+
+export const USER_TEMPORARY_TOKEN_EXPIRY: number = 5 * 60 * 1000; // 5 min
