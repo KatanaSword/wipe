905706304b0b6d4001476c9b0229502daf2bc7d0
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-20 15:17:19 +0530

    config mailgen and nodemailer, forgotPasswordMailgenContentEmail html template

---
+++ backend/src/utils/mail.ts
@@ -0,0 +1,73 @@
+import nodemailer from "nodemailer";
+import Mailgen from "mailgen";
+import { IOptions } from "../type";
+
+const sendEmail = async (options: IOptions) => {
+  // Configure mailgen by setting a theme and your product info
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      // Appears in header & footer of e-mails
+      name: "Wipe",
+      link: "https://mailgen.js/",
+      // Custom copyright notice
+      copyright: "Copyright © 2024 Wipe. All rights reserved.",
+    },
+  });
+
+  // Generate an HTML email with the provided contents
+  const emailHTML = mailGenerator.generate(options.mailgenClient);
+
+  // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+  const emailText = mailGenerator.generatePlaintext(options.mailgenClient);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: process.env.NODEMAILER_HOST, // sender address
+      to: options.email, // list of receivers
+      subject: options.subject, // Subject line
+      text: emailText, // plain text body
+      html: emailHTML, // html body
+    });
+  } catch (error) {
+    console.error(
+      "Email service failed silently. Make sure you have provided your NODEMAILER credentials in the .env file"
+    );
+    console.log("Error", error);
+  }
+};
+
+const forgotPasswordMailgenContentEmail = (
+  username: string,
+  verificationToken: string
+) => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "You have received this email because a password reset request for your account was received",
+      action: {
+        instructions: "Click the button to reset your password:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Reset your password",
+          link: verificationToken,
+        },
+      },
+      outro:
+        "If you did not request a password reset. no further action is required on your part",
+    },
+  };
+};
+
+export { sendEmail, forgotPasswordMailgenContentEmail };
