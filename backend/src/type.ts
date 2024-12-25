0b29497fe6d65af84872afbafb6523023c85e736
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-25 12:15:31 +0530

    options sendEmail parameter typecheck

--- backend/src/type.ts
+++ backend/src/type.ts
@@ -1,4 +1,5 @@
 import { Document } from "mongoose";
+import { Content } from "mailgen";
 
 export interface IUser extends Document {
   _id: string;
@@ -6,16 +7,33 @@ export interface IUser extends Document {
   email: string;
   password: string;
   avatar?: IAvatar;
-  role: string[];
+  role: string;
+  phoneNumber?: number;
+  countryCode?: string;
+  fullName?: string;
   isEmailVerify: boolean;
   refreshToken: string;
-  forgotPasswordToken: string;
-  forgotPasswordExpiry: Date;
-  emailVerificationToken: string;
-  emailVerificationExpiry: Date;
+  forgotPasswordToken?: string;
+  forgotPasswordExpiry?: Date;
+  emailVerificationToken?: string;
+  emailVerificationExpiry?: Date;
+  isPasswordCorrect(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+  generateTemporaryToken(): {
+    unHashedToken: string;
+    hashedToken: string;
+    tokenExpiry: Date;
+  };
 }
 
 type IAvatar = {
   url: string;
   publicId: string;
+};
+
+export type IOptions = {
+  email: string;
+  subject: string;
+  mailgenClient: Content;
 };
