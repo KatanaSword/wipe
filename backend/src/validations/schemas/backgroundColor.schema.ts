da8a79922f7b9cb04b8e3eb93a8c3c2b30ae0d46
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-12 11:06:57 +0530

    regex error message

--- backend/src/validations/schemas/backgroundColor.schema.ts
+++ backend/src/validations/schemas/backgroundColor.schema.ts
@@ -1,22 +1,26 @@
-import z from "zod";
+import { z } from "zod";
 
 const createBackgroundColorSchema = z.object({
   backgroundColorName: z
     .string()
-    .min(3, { message: "Background color must be at least 3 characters long" })
-    .max(20, { message: "Background color must not exceed 20 characters" }),
-  colorHexCode: z.object({
-    colorOne: z
-      .string()
-      .regex(
-        /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/
-      ),
-    colorTwo: z
-      .string()
-      .regex(
-        /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/
-      ),
-  }),
+    .min(3, {
+      message: "Background color name must be at least 3 characters long",
+    })
+    .max(15, {
+      message: "Background color name must not exceed 15 characters",
+    }),
+  colorOne: z
+    .string()
+    .regex(
+      /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/,
+      { message: "Invalid color hex code" }
+    ),
+  colorTwo: z
+    .string()
+    .regex(
+      /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/,
+      { message: "Invalid color hex code" }
+    ),
 });
 
 export { createBackgroundColorSchema };
