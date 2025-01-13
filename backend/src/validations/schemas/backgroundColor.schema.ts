494bf00025c331b5a1b357610986fc6ec281848b
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-13 11:58:55 +0530

    update bg and bg id validation schema

--- backend/src/validations/schemas/backgroundColor.schema.ts
+++ backend/src/validations/schemas/backgroundColor.schema.ts
@@ -23,4 +23,38 @@ const createBackgroundColorSchema = z.object({
     ),
 });
 
-export { createBackgroundColorSchema };
+const updateBackgroundColorSchema = z.object({
+  backgroundColorName: z
+    .string()
+    .min(3, {
+      message: "Background color name must be at least 3 characters long",
+    })
+    .max(15, {
+      message: "Background color name must not exceed 15 characters",
+    })
+    .optional(),
+  colorOne: z
+    .string()
+    .regex(
+      /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/,
+      { message: "Invalid color hex code" }
+    )
+    .optional(),
+  colorTwo: z
+    .string()
+    .regex(
+      /^#(?:[a-fA-F][0-9a-f]{2}|[a-fA-F][0-9a-f]{3}|[a-fA-F][0-9a-f]{5}|[a-fA-F][0-9a-f]{7})$/,
+      { message: "Invalid color hex code" }
+    )
+    .optional(),
+});
+
+const backgroundColorIdSchema = z.object({
+  backgroundColorId: z.string(),
+});
+
+export {
+  createBackgroundColorSchema,
+  updateBackgroundColorSchema,
+  backgroundColorIdSchema,
+};
