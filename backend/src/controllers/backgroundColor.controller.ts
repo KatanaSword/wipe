809d7bb68ad8d2abdfb273d6efbef799004d2a37
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-13 12:00:08 +0530

    update bg color controller

--- backend/src/controllers/backgroundColor.controller.ts
+++ backend/src/controllers/backgroundColor.controller.ts
@@ -1,6 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import { Request, Response } from "express";
-import { createBackgroundColorSchema } from "../validations/schemas/backgroundColor.schema";
+import {
+  backgroundColorIdSchema,
+  createBackgroundColorSchema,
+  updateBackgroundColorSchema,
+} from "../validations/schemas/backgroundColor.schema";
 import { ApiError } from "../utils/ApiError";
 import { BackgroundColor } from "../models/backgroundColor.model";
 import { ApiResponse } from "../utils/ApiResponse";
@@ -17,10 +21,10 @@ const createBackgroundColor = asyncHandler(
       throw new ApiError(400, "Field is empty", errorMessage);
     }
 
-    const backgroundColorExist = await BackgroundColor.findOne({
+    const backgroundColorNameExist = await BackgroundColor.findOne({
       backgroundColorName: parserData.data.backgroundColorName,
     });
-    if (backgroundColorExist) {
+    if (backgroundColorNameExist) {
       throw new ApiError(
         409,
         "Background color already exist, use another name"
@@ -59,7 +63,59 @@ const getBackgroundColorById = asyncHandler(
 );
 
 const updateBackgroundColor = asyncHandler(
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const parserData = updateBackgroundColorSchema.safeParse(req.body);
+    console.log("ParserData:", parserData);
+    const parserId = backgroundColorIdSchema.safeParse(req.params);
+    console.log("ParserId:", parserId);
+    const errorMessage = parserData.error?.issues.map((issue) => issue.message);
+    console.log("ErrorMessage:", errorMessage);
+    if (!parserData.success || !parserId.success) {
+      throw new ApiError(400, "", errorMessage);
+    }
+
+    const backgroundColorNameExist = await BackgroundColor.findOne({
+      backgroundColorName: parserData.data.backgroundColorName,
+    });
+    console.log("BackgroundColorNameExist:", backgroundColorNameExist);
+    if (backgroundColorNameExist) {
+      throw new ApiError(
+        409,
+        "Background color name already exist, try other name"
+      );
+    }
+
+    const updateBackgroundColor = await BackgroundColor.findByIdAndUpdate(
+      parserId.data.backgroundColorId,
+      {
+        $set: {
+          backgroundColorName: parserData.data.backgroundColorName,
+          colorHexCode: {
+            colorOne: parserData.data.colorOne,
+            colorTwo: parserData.data.colorTwo,
+          },
+        },
+      },
+      { new: true }
+    );
+    console.log("UpdateBackgroundColor:", updateBackgroundColor);
+    if (!updateBackgroundColor) {
+      throw new ApiError(
+        500,
+        "Background color update failed, Please try again later"
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          updateBackgroundColor,
+          "Background color update successfully"
+        )
+      );
+  }
 );
 
 const removeBackgroundColorImage = asyncHandler(
