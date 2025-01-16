7c33cbb6f12b2721859e8f1c4f009e6fe01e0230
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-16 11:17:20 +0530

    aspect ratio update and id  validation schema

--- backend/src/validations/schemas/aspectRatio.schema.ts
+++ backend/src/validations/schemas/aspectRatio.schema.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import { z } from "zod";
 
 const createAspectRatioSchema = z.object({
   aspectRatioName: z
@@ -7,14 +7,28 @@ const createAspectRatioSchema = z.object({
     .trim()
     .min(4, { message: "Name must be at least 4 characters long" })
     .max(20, { message: "Name must not exceed 20 characters" }),
-  height: z
-    .number()
-    .min(200, { message: "Height must be at least 200 pixel" })
-    .max(1080, { message: "Height must not exceed 1080 pixel" }),
-  width: z
-    .number()
-    .min(200, { message: "Width must be at least 200 pixel" })
-    .max(1080, { message: "Width must not exceed 1080 pixel" }),
+  height: z.number(),
+  width: z.number(),
 });
 
-export { createAspectRatioSchema };
+const updateAspectRatioSchema = z.object({
+  aspectRatioName: z
+    .string()
+    .toLowerCase()
+    .trim()
+    .min(4, { message: "Name must be at least 4 characters long" })
+    .max(20, { message: "Name must not exceed 20 characters" })
+    .optional(),
+  height: z.number().optional(),
+  width: z.number().optional(),
+});
+
+const aspectRatioIdSchema = z.object({
+  aspectRatioId: z.string(),
+});
+
+export {
+  createAspectRatioSchema,
+  updateAspectRatioSchema,
+  aspectRatioIdSchema,
+};
