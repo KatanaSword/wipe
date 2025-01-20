cd488721112d75ea677605c42f00a5cf01cab3c1
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-20 12:30:29 +0530

    declear blog router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,10 +20,12 @@ app.use(cookieParser());
 import userRouter from "./routers/user.router";
 import backgroundColorRouter from "./routers/backgroundColor.router";
 import aspectRatioRouter from "./routers/aspectRatio.router";
+import blogRouter from "./routers/blog.router";
 
 // declear routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/background_colors", backgroundColorRouter);
 app.use("/api/v1/aspect_ratios", aspectRatioRouter);
+app.use("/api/v1/blogs", blogRouter);
 
 export default app;
