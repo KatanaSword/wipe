d3735492d29a66fbdf7b8124c6326b6116e3c0e0
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-26 09:56:04 +0530

    change db and orm

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,12 +1,14 @@
-import { drizzle } from "drizzle-orm/mysql2";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    const connectionInstance = drizzle(
+    const connectionInstance = await mongoose.connect(
       `${process.env.DATABASE_URL}/${DB_NAME}`
     );
-    console.log(`\n MySQL connected !! DB Host: ${connectionInstance}`);
+    console.log(
+      `\nMySQL connected !! DB Host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MySQL connection failed!!!:", error);
     process.exit(1);
