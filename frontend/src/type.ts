f8019b78c9a362887a865ad3bcbb7d28e9bb919d
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-17 04:02:35 +0530

    typecheck

--- frontend/src/type.ts
+++ frontend/src/type.ts
@@ -27,8 +27,50 @@ export type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
 };
 
 export type TextProps = React.HTMLAttributes<HTMLElement> & {
-  children: string;
+  children: React.ReactNode;
   className?: string;
   size?: string | number;
-  as?: "span" | "p" | "div";
+  as?: "span" | "p" | "div" | "h1" | "h2" | "h3";
+};
+
+export type LineProps = React.HTMLAttributes<HTMLDivElement> & {
+  className?: string;
+};
+
+export type PricingPoint = {
+  id: number;
+  point: string;
+};
+
+export type Services = {
+  svg: React.ReactNode;
+  title: string;
+  description: string;
+  link: string;
+};
+
+export type FAQS = {
+  id: number;
+  question: string;
+  answer: string;
+};
+
+export type Testimonials = {
+  review: string;
+  name: string;
+  occupation: string;
+  profile: string;
+};
+
+export type Theme = "dark" | "light" | "system";
+
+export type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+export type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 };
