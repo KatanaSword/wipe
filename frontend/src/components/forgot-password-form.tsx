6b8830883e73e83653ffd9738eab15a9aeaf6161
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-19 10:08:54 +0530

    style: forgot password form component

---
+++ frontend/src/components/forgot-password-form.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Text } from "./ui";
+import { Link } from "react-router";
+
+export default function ForgotPasswordForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div className="mx-auto w-full max-w-md rounded-xl bg-white p-4 dark:bg-black md:rounded-2xl md:p-8">
+      <h2 className="mb-4 text-center text-xl font-bold text-neutral-800 dark:text-neutral-200">
+        Forgot Password?
+      </h2>
+      <p className="mt-2 max-w-sm text-center text-sm text-neutral-600 dark:text-neutral-300">
+        Login to panel if you can because we don&apos;t have a login flow yet
+      </p>
+
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          </LabelInputContainer>
+        </div>
+
+        <button
+          className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-purple-P400 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+          type="submit"
+        >
+          Send
+          <BottomGradient />
+        </button>
+
+        <Text className="mb-8 mt-16 text-center text-sm text-black dark:text-white">
+          Or you can{" "}
+          <Link
+            to="/signin"
+            className="text-orange-O300 text-sm font-semibold underline"
+          >
+            Sign in
+          </Link>
+        </Text>
+      </form>
+    </div>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex w-full flex-col space-y-2", className)}>
+      {children}
+    </div>
+  );
+};
