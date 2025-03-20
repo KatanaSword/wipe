554f0453fd0d908d758483ad79aa7d5062560085
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-20 23:32:21 +0530

    typecheck for handle api

--- frontend/src/type.ts
+++ frontend/src/type.ts
@@ -1,3 +1,5 @@
+// React Component Type
+
 export type SVGProps = React.SVGProps<SVGSVGElement> & {
   fillColor?: string;
   height?: number | string;
@@ -74,3 +76,114 @@ export type ThemeProviderState = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 };
+
+export type InitialState = {
+  status: boolean;
+  userData: null;
+};
+
+// ENV Config Type
+
+export type Conf = {
+  backendUserUrl: string;
+  backendBackgroundColorUrl: string;
+  backendAspectRatioUrl: string;
+  backendBlogUrl: string;
+  backendCodeUrl: string;
+  backendScreenshotUrl: string;
+  backendTestimonialUrl: string;
+};
+
+// Auth Type
+
+export interface Register {
+  username: string;
+  email: string;
+  password: string;
+  role?: string;
+}
+
+export interface Signin {
+  username?: string;
+  email?: string;
+  password: string;
+}
+
+export interface AccountDetailUpdate {
+  phoneNumber?: number;
+  countryCode?: string;
+  fullName?: string;
+}
+
+export interface AssignRole {
+  userId: string;
+  role: string;
+}
+
+export interface ChangePassword {
+  currentPassword: string;
+  newPassword: string;
+}
+
+export interface ResetPassword {
+  token: string;
+  resetPassword: string;
+}
+
+// Aspect Ratio Type
+
+export interface CreateAspectRatio {
+  aspectRatioName: string;
+  height: number;
+  width: number;
+}
+
+export interface UpdateAspectRatio {
+  aspectRatioName?: string;
+  height?: number;
+  width?: number;
+}
+
+// Background Color Type
+
+export interface CreateBackgroundColor {
+  backgroundColorName: string;
+  colorOneHexCode: string;
+  colorTwoHexCode: string;
+}
+
+export interface UpdateBackgroundColor {
+  backgroundColorName?: string;
+  colorOneHexCode?: string;
+  colorTwoHexCode?: string;
+}
+
+// Blog Type
+
+export interface CreateBlogPost {
+  fileName: string;
+  title: string;
+  summary: string;
+  aspectRatioName: string;
+  backgroundColorName: string;
+}
+
+export interface UpdateBlogPost {
+  title?: string;
+  summary?: string;
+}
+
+export interface BlogPostFileName {
+  fileName?: string;
+  blogId: string;
+}
+
+export interface BlogPostAspectRatio {
+  aspectRatioName?: string;
+  blogId: string;
+}
+
+export interface BlogPostBackgroundColor {
+  backgroundColorName?: string;
+  blogId: string;
+}
